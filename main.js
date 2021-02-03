a31a78b63bbb09374737bff5e1588cc3d47e39ac
Patryk Polus <poluspatryk.deweloper@gmail.com>
2021-02-03 01:29:30 +0100

    Stworzenie JS

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+// let people = ["Mikołaj", 12, true]
+
+// console.log(people.length)
+
+// for(let i = 0; i < people.length; i++){
+//     console.log(people[i])
+// }
+
+// let worker = {
+//     name: "Ania",
+//     age : 35,
+// }
+
+// console.log(worker.name)
+
+// const children = [
+//     {
+//         name: "Ania",
+//         age: 3
+//     },
+//     {
+//         name: "Krzysio",
+//         age: 2
+//     }
+// ]
+
+// for(let i = 0; i < children.length; i++) {
+//     console.log(children[i].name)
+// }
+
+// function sum(a , b) {
+//     return a + b
+// }
+
+// const sum = (a, b) => {
+//     return a + b
+// } 
+
+// sum(4, 5)
+
+// console.log(sum)
+
+// const calc = (a, b, c) => {
+//     if(c === 0 || b === 0 || a === 0) {
+//         if(a === 0) {
+//             a = 1
+//         }
+//         if(b === 0) {
+//             b = 1
+//         }
+//         if(c === 0) {
+//             c = 1
+//         }
+//     }
+//     console.log(a + b / c)
+// }
+
+// calc(0, 0, 0)
+
+function createPople() {
+   console.log("kliknąłem 3")
+}
